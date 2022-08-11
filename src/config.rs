13c76a7931fa2cf600fbcfabee48acb0a6db945c
Serge Barral <serge.barral@asynchronics.com>
2022-08-11 13:52:39 +0200

    Fix typo in comments

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 use crate::loom_types::sync::atomic;
 
-// If the target is 64-bit or the `long_counter` feature is selected (which is
-// the default), use u64 as a long integer and u16 as short integer.
+// If the target is 64-bit wide or the `long_counter` feature is selected (which
+// is the default), use u64 as a long integer and u32 as short integer.
 #[cfg(any(target_pointer_width = "64", feature = "long_counter"))]
 pub(crate) type UnsignedShort = u32;
 #[cfg(any(target_pointer_width = "64", feature = "long_counter"))]
@@ -11,7 +11,7 @@ pub(crate) type AtomicUnsignedShort = atomic::AtomicU32;
 #[cfg(any(target_pointer_width = "64", feature = "long_counter"))]
 pub(crate) type AtomicUnsignedLong = atomic::AtomicU64;
 
-// Otherwise use u32 as long integer and u8 as short integer.
+// Otherwise use u32 as long integer and u16 as short integer.
 #[cfg(not(any(target_pointer_width = "64", feature = "long_counter")))]
 pub(crate) type UnsignedShort = u16;
 #[cfg(not(any(target_pointer_width = "64", feature = "long_counter")))]
